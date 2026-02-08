46a24757ab446015371ef892f2b31099e372217e
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2026-02-07 23:32:20 -0600

    docs: update title and description for KMP Handbook
    Updated the title and description in the configuration files to reflect
    the correct branding for the KMP Handbook. This change ensures that
    the documentation accurately represents the project and provides
    users with relevant information about the handbook's purpose.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'KMP Handbook',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kevinah95/KMPHandbook' }],
 			sidebar: [
 				{
 					label: 'Guides',
